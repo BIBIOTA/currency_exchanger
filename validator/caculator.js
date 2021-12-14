22b49486770ee4b553ca53f6e87b96191975f130
BiBiOTA <yukiotataitien@gmail.com>
2021-12-14 17:03:05 +0800

    -- fixed -- 算錢用浮點 遲早被人扁

--- validator/caculator.js
+++ validator/caculator.js
@@ -1,4 +1,6 @@
+import { chain } from 'mathjs'
+
 export const currencyCaculator = (amount, buyRate, sellRate) => {
-  const caculateAmount = amount * buyRate * sellRate;
+  const caculateAmount = chain(amount).multiply(buyRate, sellRate);
   return Math.round((caculateAmount + Number.EPSILON) * 100) / 100;
 }
